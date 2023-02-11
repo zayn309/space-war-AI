fba93f3a4a2025c5d3789ecb0406f852ad29bafd
zayn309 <joozain309@gmail.com>
2023-02-11 06:49:15 +0200

    separated the handle_colision method in the game class into multiple methodes

--- Game.py
+++ Game.py
@@ -1,4 +1,3 @@
-
 import pygame
 import os
 from spaceship import *
@@ -35,49 +34,52 @@ YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
         RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-class gameInfo:
-    pass
-
 class Game:
+    
     def __init__(self,screen):
         self.yellow = leftSpaceShip(YELLOW_SPACESHIP,100,300,YELLOW)
         self.red = rightSpaceShipe(RED_SPACESHIP,700,300,RED)
         self.screen= screen
         self.winner_text = ""
         
-        
-    def handle_collision(self):
+    def red_bullet_collision(self):
         for bullet in self.yellow.bullets:
             if self.red.rect.colliderect(bullet):
                 self.red.health -=1
                 self.yellow.bullets.remove(bullet)
+                return True
+            
+    def yellow_bullet_collision(self):
         for bullet in self.red.bullets:
             if self.yellow.rect.colliderect(bullet):
                 self.yellow.health -=1
                 self.red.bullets.remove(bullet)
-        
+                return True
+            
+    def yellow_award_collision(self):
         for a in self.yellow.award:
             if self.yellow.rect.colliderect(a):
                 a.action(self.yellow)
                 self.yellow.award.remove(a)
-        
+                return True
+            
+    def red_award_collision(self):
         for a in self.red.award:
             if self.red.rect.colliderect(a):
                 a.action(self.red)
                 self.red.award.remove(a)
-        
-    
-
+                return True
+            
     def draw_health(self):
         red_health_text = HEALTH_FONT.render(
-            "Health: " + str(self.red.health), 1, WHITE)
+            "Health: " + str(self.red.health), True, WHITE)
         yellow_health_text = HEALTH_FONT.render(
-            "Health: " + str(self.yellow.health), 1, WHITE)
+            "Health: " + str(self.yellow.health), True, WHITE)
         self.screen.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
         self.screen.blit(yellow_health_text, (10, 10))
 
     def draw_winner(self):
-        draw_text = WINNER_FONT.render(self.winner_text, 1, WHITE)
+        draw_text = WINNER_FONT.render(self.winner_text, True, WHITE)
         self.screen.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
                             2, HEIGHT/2 - draw_text.get_height()/2))
         pygame.display.update()
@@ -88,9 +90,11 @@ class Game:
         self.red.move(keys_pressed)
         self.yellow.move_bullets()
         self.red.move_bullets()
-        self.handle_collision()
-        
-    
+        self.red_bullet_collision()
+        self.red_award_collision()
+        self.yellow_bullet_collision()
+        self.yellow_award_collision()
+
     def rais_winner(self):
         if self.red.health <= 0:
             self.winner_text = "Yellow Wins!"
